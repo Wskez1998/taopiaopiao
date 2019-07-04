a0249b4ec6c11ebcfb94b50d62f4f6039e364695
caiyiii <caiyii4455@aliyun.com>
2019-07-04 21:33:27 +0800

    后台user模型

---
+++ server/index.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const multer = require('multer')
+const update = multer({
+    dest: './tmp'
+});
+const url = "mongodb://127.0.0.1:27017/taopp"
+//引入controllers
+const userController = require('./controllers/user')
+const server = express()
+//连接 mongodb 
+mongoose
+    .connect(url, { useNewUrlParser: true })
+    .then(() => {
+        console.log("连接数据库成功 🤟")
+    })
+    .catch(error => {
+        console.log(error)
+        console.log("连接数据库失败 ☠")
+    })
+
+//各种中间件的使用与配置
+server.use(express.json());
+server.use(express.urlencoded({ extended: false }));
+server.use(express.static("public"));
+server.use((req, res, next) => {
+  res.set("Access-Control-Allow-Origin", "*");
+  res.set("Access-Control-Allow-Headers", "content-type");
+  next();
+});
+
+//路由的设置
+server.post("/sign-up", userController.postSignUp); // 注册
+server.post("/sign-in", userController.postSignIn); // 登录
+server.post(
+  "/user/profile",
+  update.single("avatar"),
+  userController.postUpdAvatar
+);
+
+server.listen(7777)
---
+++ server/models/User.js
@@ -0,0 +1,26 @@
+const mongoDB = require('mongoose')
+
+const userSchema = new mongoDB.Schema({
+    username: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    nickname: {
+        type: String,
+        default: '桃子星人'
+    },
+    avatar: {
+        type: String,
+        default: 'http://localhost:9090/avatar.jpg'
+    },
+    relos: {
+        type: Number,
+        default: 2
+    }
+})
+
+module.exports = mongoDB.model("user", userSchema)
