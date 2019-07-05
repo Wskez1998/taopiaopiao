4a0ca45d5f6ad3f323503d87e54d269830b92a1a
Wskez1998 <2209027508@qq.com>
2019-07-05 09:06:47 +0800

    合并冲突

--- src/main.js
+++ src/main.js
@@ -2,11 +2,7 @@ import Vue from "vue";
 import router from "./router";
 import store from "./store/index";
 import App from "./App";
-<<<<<<< HEAD
-import { Tab, Tabs, Toast, List, Search, DropdownMenu, DropdownItem  } from "vant";
-=======
-import { Tab, Tabs, Toast, List, Search, Field, CellGroup } from "vant";
->>>>>>> 0d072575496c93d4a912c8d577c0fb61e19ba575
+import { Tab, Tabs, Toast, List, Search, DropdownMenu, DropdownItem ,Field, CellGroup } from "vant";
 import "./assets/styles/base.scss";
 
 Vue.use(Tab)
@@ -14,13 +10,10 @@ Vue.use(Tab)
   .use(Toast)
   .use(List)
   .use(Search)
-<<<<<<< HEAD
   .use(DropdownMenu)
-  .use(DropdownItem);
-=======
+  .use(DropdownItem)
   .use(Field)
   .use(CellGroup);
->>>>>>> 0d072575496c93d4a912c8d577c0fb61e19ba575
 
 new Vue({
   el: "#app",
